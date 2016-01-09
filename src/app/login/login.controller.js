8360a2410cbb62f82a777dbd934dbe2f6330f2a8
Krzysztof Kuśmierczyk <krzysztof.kusmierczyk@outlook.com>
2016-01-09 15:06:22 +0100

    not working login

--- src/app/login/login.controller.js
+++ src/app/login/login.controller.js
@@ -5,15 +5,26 @@
         .module('yoothub')
         .controller('LoginController', LoginController);
 
-    LoginController.$inject = [];
-    function LoginController() {
+    LoginController.$inject = ['AccountService'];
+    function LoginController(AccountService) {
         var vm = this;
-
+        vm.schemas = [];
+        vm.useSchema = useSchema;
 
         activate();
 
         ////////////////
 
-        function activate() { }
+        function activate() {
+            AccountService.getAuthSchemas().then(setSchemas);
+        }
+
+        function setSchemas(data) {
+            vm.schemas = data;
+        }
+
+        function useSchema(schema) {
+            AccountService.logInWithSchema(schema);
+        }
     }
 })();
--- src/app/services/account.service.js
+++ src/app/services/account.service.js
@@ -8,7 +8,9 @@
     AccountService.$inject = ['$http'];
     function AccountService($http) {
         var service = {
-            getAuthStatus: getAuthStatus
+            getAuthStatus: getAuthStatus,
+            getAuthSchemas: getAuthSchemas,
+            logInWithSchema: logInWithSchema
         };
 
         return service;
@@ -16,15 +18,36 @@
         ////////////////
         
         
-        function getAuthStatus() {
+        function getAuthStatus() {// TODO: extract urls
             var url = '/api/Account/ExternalLoginCallback';
 
-            return $http.get(url).then(checkAuthenticatedStatusCode);
-
+            return $http.get(url).then(getAuthenticatedStatusCode);
         }
 
-        function checkAuthenticatedStatusCode(response) {
+        function getAuthenticatedStatusCode(response) {
             return response.status;
         }
+
+        function getAuthSchemas() {
+            var url = '/api/Account/GetExternalAuthenticationSchemes';
+
+            return $http.get(url).then(getData);
+        }
+
+        function getData(response) {
+            return response.data;
+        }
+
+        function logInWithSchema(schema) {
+            var url = '/api/Account/ExternalLogin';
+            
+            $http.post(url, schema.AuthenticationScheme).then(logResponse);// redirect
+        }
+        
+        function logResponse(response){
+            
+            console.log(response);
+        }
+
     }
 })();
