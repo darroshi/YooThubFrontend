c78b80dbba94a6ffa7d023198c0d8dfb4f06ddd2
Krzysztof Kuśmierczyk <krzysztof.kusmierczyk@cohesiva.com>
2016-02-19 12:52:19 +0100

    remove materilize, install angularm aterial, clean build.js, fix sass error

--- gulp/build.js
+++ gulp/build.js
@@ -65,9 +65,7 @@ gulp.task('html', ['inject', 'partials'], function () {
     }))
     .pipe(htmlFilter.restore)
     .pipe(gulp.dest(path.join(conf.paths.dist, '/')))
-    .pipe($.size({ title: path.join(conf.paths.dist, '/'), showFiles: true }))
-    .pipe($.replace('../bower_components/Materialize/font/roboto', 'fonts'))
-    .pipe($.replace('../bower_components/Materialize/font/material-design-icons', 'fonts'));
+    .pipe($.size({ title: path.join(conf.paths.dist, '/'), showFiles: true }));
   });
 
 // Only applies for fonts from bower dependencies
