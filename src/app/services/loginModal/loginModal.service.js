e70f0046b91a545199451eba04e8f82103ece348
Krzysztof Kuśmierczyk <krzysztof.kusmierczyk@outlook.com>
2016-03-19 14:15:08 +0100

    fix login modal, still needs some work

--- src/app/services/loginModal/loginModal.service.js
+++ src/app/services/loginModal/loginModal.service.js
@@ -1,6 +1,42 @@
-/* global LoginModalController */
 (function() {
     'use strict';
+
+    angular
+        .module('yoothub')
+        .controller('LoginModalController', LoginModalController);
+
+    LoginModalController.$inject = ['AccountService', '$log', '$window', '$rootScope', '$scope'];
+    function LoginModalController(AccountService, $log, $window, $rootScope, $scope) {
+        var vm = this;
+        vm.schemas = [];
+        vm.formAction = '/api/Account/ExternalLogin';
+        vm.previousUrl = '/' + $window.location.hash;
+
+        activate();
+
+        ////////////////
+        var destroyLocationChangedHandler = null;
+
+        function activate() {
+            $log.debug('Activate LoginModalController');
+            destroyLocationChangedHandler = $rootScope.$on('$locationChangeSuccess', locationChanged);
+
+            $scope.$on('$destroy', function() {
+                destroyLocationChangedHandler();
+            });
+
+            AccountService.getAuthSchemas().then(setSchemas);
+        }
+
+        function locationChanged() {
+            vm.previousUrl = '/' + $window.location.hash;
+        }
+
+        function setSchemas(data) {
+            $log.debug('Set schema ', data);
+            vm.schemas = data;
+        }
+    }
 
     angular
         .module('yoothub')
@@ -26,3 +62,5 @@
 
     }
 })();
+
+
